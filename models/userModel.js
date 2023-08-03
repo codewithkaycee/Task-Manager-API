c5020916dfc4d92ace2aed099e9a91673f31f830
Kaycee Nuel <heykayceee@gmail.com>
2023-08-03 04:26:44 +0100

    Update Userschema.signup to hash and store passwords securely

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
+const { genSalt } = require('bcrypt');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -12,5 +14,21 @@ const Userschema = new mongoose.Schema({
         type:String,
         required:true,
     }
-})
+});
+
+//signup static method. 
+Userschema.statics.signup = async function (email, password){
+    const existingEmail= await this.findOne( {email})
+
+    if (existingEmail){
+        throw Error ('Email already exists');
+    }
+    //salt password 
+    const saltPassWord = await bcrypt.genSalt(10);
+    //hash password
+    const hashedPassWord = await bcrypt.hash(password, saltPassWord);
+
+    const user = await this.create ({email, password: hashedPassWord})
+    return user;
+}
 module.exports = mongoose.model('User', Userschema)
