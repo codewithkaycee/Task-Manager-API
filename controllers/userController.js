e031cdb9c64e188cad7646952c65345f3f2494b9
Kaycee Nuel <heykayceee@gmail.com>
2023-08-04 23:52:33 +0100

    Implement JWT token handling in usercontroller.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,10 @@
-const User = require('../models/userModel')
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
+// generate a token key 
+const generateTokenKey = (_id) =>{
+  return jwt.sign({_id}, process.env.SECRET_KEY,{expiresIn:'1h'});
+}
 //login user controller 
 const loginUser = async (req,res) => {
   res.json({mssg: 'login user'})
@@ -12,8 +17,11 @@ const signupUser = async(req,res) => {
 
   try {
     const user = await User. signup(email,password);
+
+    // create token 
+    const token = generateTokenKey(user._id)
     // Res with a successful status and the user's email
-    res.status(200).json({email, user})
+    res.status(200).json({email, token})
     // If an error occurs, res with an error status and the error message
   } catch (error) {
     res.status(400).json({error: error.message})
