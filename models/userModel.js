fc87b62448b5dff1106963e2d574753093c0bd8c
Kaycee Nuel <heykayceee@gmail.com>
2023-08-08 00:43:02 +0100

    Add login Authentication for user

--- models/userModel.js
+++ models/userModel.js
@@ -43,4 +43,20 @@ Userschema.statics.signup = async function (email, password){
     const user = await this.create ({email, password: hashedPassWord})
     return user;
 }
+
+      //login static method
+      Userschema.statics.login = async function (email, password ){
+     //check if fields are filled 
+     if(!email || !password){
+        throw Error('All fields must be filled.')
+        }; 
+        const user = await this.findOne({email})
+        if (!user){
+            throw new Error("Invalid email or password");
+        };
+        const matchPassword = await bcrypt.compare (password , user.password)
+        if (!matchPassword){
+            throw new Error ("Invalid Password")
+        }
+ }
 module.exports = mongoose.model('User', Userschema) 
